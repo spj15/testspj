bbbfcb0fd5b9da994c07928b93f2bda7b1cca127
s <saurabhpjaiswal@gmail.com>
2024-10-21 11:35:12 -0700

    bad code

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,9 +15,12 @@ public class MyStack {
 
     public int pop() {
         if(list.size() == 0) {
+            System.out.println("Can't pop empty stack");
             return -1;
         } else {
-            size = size - 1;
+
+            size--;
+            System.out.println("size value" + size);
             return list.remove(list.size() - 1);
         }
     }
@@ -24,12 +28,16 @@ public class MyStack {
 
     public int push(int value) {
 
+        // stack overflow
 
-        if(list.size() == size) {
+        System.out.println("size value" + size);
+        if(list.size() > size) {
+            System.out.println("Stack overflow");
             return -1;
         }
+        System.out.println("list size" + list.size());
         list.add(value);
-        size++;
+        //size++;
         return value;
     }
 
@@ -55,6 +63,17 @@ public class MyStack {
         for(int i = 0; i < 5; i++) {
             System.out.println(stack.pop()); // 5
         }
+
+        //System.out.println("stack");
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        stack.push(7);
+
 
 
 
